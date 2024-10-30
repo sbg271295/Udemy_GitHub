7c115b49d57406363eebfbf344538589c6ae44f7
Sergio Bravo <s.bravoga1995@gmail.com>
2024-10-30 11:05:13 +0100

    actualizacion

--- src/service/CalculadoraService.java
+++ src/service/CalculadoraService.java
@@ -9,5 +9,14 @@ public class CalculadoraService {
 	public int restar(int a, int b) {
 		return a-b;
 	}
+	
+	public int multiplicar(int a, int b) {
+		
+		return a*b;
+	}
+	public int dividir(int a, int b) {
+		
+		return a/b;
+	}
 
 }
